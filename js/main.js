64b2370351e84fbea0e8e1dd7b1b0b40ca5c4979
dayanaalvarado <dayana.alvarado.n@gmail.com>
2016-10-24 14:14:39 -0300

    todo

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function traduccion(){
+
+document.getElementById('form-signin-heading').innerHTML="Por favor iniciar sesión";
+document.getElementById('inputEmail').placeholder="Correo Electrónico";
+document.getElementById('inputPassword').placeholder="Contraseña";
+document.querySelector('button').innerHTML="Iniciar Sesión";
+document.querySelector('span').innerHTML="Recordar Datos";
+
+
+
+}
+
+
